974a736d1bb6cf0a718f77325628b6ac339f7e29
Sean Choi <seanchoi@seans-mac-mini.myfiosgateway.com>
2020-11-18 12:46:23 -0500

    django model practice

---
+++ mysite/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html')),
+    path('photos/', include('mysite.photos.urls', namespace="photos")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
